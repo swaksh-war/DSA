d977e2de80cb83e4dfe4c0ae269cb922b2b6662b
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2021-08-22 12:14:22 +0530

    Stack Implementation In python

---
+++ stack.py
@@ -0,0 +1,21 @@
+class Stack:
+    def __init__(self, data):
+        self.data = data
+    
+    
+    def push(self, data):
+        self.data.insert(0,data)
+    
+    def pop(self):
+        return self.data.pop(0)
+    
+    def top(self):
+        return self.data[0]
+    
+    def __len__(self):
+        return len(self.data)
+    
+    
+    def __str__(self):
+        return (''.join('{}\n'.format(i) for i in self.data))
+            
